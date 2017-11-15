49145c1a59795b5ff49a99afef59591ec926d725
Carlos Mota <CarlosMota@protonmail.com>
2017-11-14 21:01:43 -0500

    SearchFilter added
    added the process.argv which reads what the user puts in the terminal

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const https = require('https');
 
 //* https://swapi.co/api/option1/?search=option2
 
+const searchFilter = process.argv.slice(2); /* When the user writes something in the terminal
+                                               we capture that and send it to  searchFilter */
 const printMessage = (category, search) => {
 
 }
