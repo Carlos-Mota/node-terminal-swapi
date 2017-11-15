d14eb32dadec69cd991171003908aa8f8065b83a
Carlos Mota <CarlosMota@protonmail.com>
2017-11-14 20:55:22 -0500

    creating the print message
    Print Message creation and importing the https

---
+++ app.js
@@ -0,0 +1,10 @@
+//* Making a simple way to connect to SWAPI "https://swapi.co/api/"
+//? Using nodeJS to SWAPI API to get user input and display the info
+
+const https = require('https');
+
+//* https://swapi.co/api/option1/?search=option2
+
+const printMessage = (category, search) => {
+
+}
