2504fcf6b351630b2bbda611f1bd2c7edfc04da1
Carlos Mota <CarlosMota@protonmail.com>
2017-11-23 23:43:11 -0500

    People search Done
    this is for people searching with the api and it works.

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ const https = require('https');
 
 const searchFilter = process.argv.slice(2); /* When the user writes something in the terminal
                                                we capture that and send it to  searchFilter */
-const printMessage = (category, search) => {
+                                              
+const getSearch = (category, search) => {
+    //* we are gonna use the api call 
+    //? this is the api url "https://swapi.co/api/people/?search=Luke                               
+    const request = https.get(`https://swapi.co/api/${searchFilter[0]}/?search=${searchFilter[1]}`,
+    response => {
+    // Read the data
+    
+    let body = "";
+    response.on("data", data => {
+        body += data.toString();
+        });
+        
+    response.on("end", () => {
+        const info = JSON.parse(body);
+         printMessage(info.results[0].name, info.results[0].mass);
+        });
+    });
+}
 
+getSearch(searchFilter[0], searchFilter[1]);
+
+const printMessage = (name, mass) => {
+    console.log(`This is ${name}, ${name} has the following mass of ${mass}`);
 }
