188d2ce90331e2e083e58e4b4a67bbab212693a7
Carlos Mota <CarlosMota@protonmail.com>
2017-11-26 18:09:52 -0500

    I've added request
    I've switched to express and also added request to make http calls
    I've also stopped using res.send and now use res.status

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get(`/`, (req, res) => {
+    request('https://teamtreehouse.com/carlosmota2.json', { json: true }, (error, response, body) => {
+        res.status(200).send(body); //! this is using express and request npm install
+    });
 });
 
 app.listen(3000,() => {
